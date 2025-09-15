b9f87f1add2677bd96b2369fbd1f200b668637aa
Julia <maydeli.castan@iest.edu.mx>
2025-09-15 16:18:51 -0600

    Initial Commit

---
+++ ExamenPrimerParcial20291/ExamenPrimerParcial20291App.swift
@@ -0,0 +1,17 @@
+//
+//  ExamenPrimerParcial20291App.swift
+//  ExamenPrimerParcial20291
+//
+//  Created by WIN603 on 15/09/25.
+//
+
+import SwiftUI
+
+@main
+struct ExamenPrimerParcial20291App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
